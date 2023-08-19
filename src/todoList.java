2b8e417a780f0206c1e8147d9cf1c5215227b0df
LuisLeal123 <lal10@rice.edu>
2023-08-19 02:43:36 -0500

    -commit

--- src/todoList.java
+++ src/todoList.java
@@ -8,84 +8,98 @@ import java.util.Arrays;
  Create a simple to-do list application that allows users to add,
  delete, and mark tasks as complete.
  */
-// current issue to fix: if complete, they can only be deleted
-// if you select a complete task, the non-complete tasks gray out
-public class mainGUI extends JFrame {
+
+public class todoList extends JFrame {
     // checklist
     private JCheckBox[] checkboxes;
     // array list which contains the tasks
     private final ArrayList<String> tasks;
 
     // Constructor to open the GUI
-    public mainGUI(String[] items) {
+    public todoList(String[] items) {
         // sets the name for the window using a JFrame constructor
         super("Checklist GUI");
-        // closes the GUI instead of keeping it running
-        // JFrame inherits setDefaultCloseOperation from WindowConstants interface
+        // closes the GUI instead of keeping it running (JFrame inherits setDefaultCloseOperation from WindowConstants interface)
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // initialize it as an instance
-        // add tasks from input to this instance
+        // create instance variables of the 'tasks' & 'checkboxes'
         tasks = new ArrayList<>();
         tasks.addAll(Arrays.asList(items));
-
-        // initialize array as length of tasks in input
         checkboxes = new JCheckBox[items.length];
         JPanel panel = new JPanel();
+        // uses grid layout: 1 column & n+3 rows
         panel.setLayout(new GridLayout(items.length + 3, 1));
 
+        // fills the 'checkboxes' instance variable & fills the panel with JCheckBoxes
         for (int i = 0; i < items.length; i++) {
             checkboxes[i] = new JCheckBox(items[i]);
             panel.add(checkboxes[i]);
         }
 
+
+        // code for the add task button
         JButton addButton = new JButton("Add Task");
         addButton.addActionListener(e -> {
-            String newTask = JOptionPane.showInputDialog(mainGUI.this, "Enter the new task:");
+            // prompts user to type out a new task
+            String newTask = JOptionPane.showInputDialog(todoList.this, "Enter the new task:");
+            // is now added to new task if it is not null or is not just blank space
             if (newTask != null && !newTask.trim().isEmpty()) {
                 tasks.add(newTask);
+                // checkboxes don't update without this
                 updateCheckboxes();
             }
         });
 
+
+        // code for the delete task button
         JButton deleteButton = new JButton("Delete Selected Tasks");
         deleteButton.addActionListener(e -> {
+            // counts the tasks selected
             int numChecked = 0;
-            for (int i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].isSelected()) {
+            for (JCheckBox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
                     numChecked++;
                 }
             }
+            // if selected
             if (numChecked > 0) {
-                int confirm = JOptionPane.showConfirmDialog(mainGUI.this,
+                // shows yes/no dialog box to confirm a deletion
+                int confirm = JOptionPane.showConfirmDialog(todoList.this,
                         "Are you sure you want to delete the selected task(s)?",
                         "Confirm Deletion", JOptionPane.YES_NO_OPTION);
                 if (confirm == JOptionPane.YES_OPTION) {
+                    // add tasks to be deleted into a list
                     ArrayList<String> tasksToDelete = new ArrayList<>();
                     for (int i = 0; i < checkboxes.length; i++) {
                         if (checkboxes[i].isSelected()) {
                             tasksToDelete.add(tasks.get(i));
                         }
                     }
+                    // deletes all tasks
                     tasks.removeAll(tasksToDelete);
                     updateCheckboxes();
                 }
+                // if no boxes were selected, shows a dialog box
             } else {
-                JOptionPane.showMessageDialog(mainGUI.this,
+                JOptionPane.showMessageDialog(todoList.this,
                         "Please select tasks to delete.", "No Tasks Selected", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
+
+        // code for the 'mark task as complete' button
         JButton completeButton = new JButton("Mark Selected Tasks as Complete");
         completeButton.addActionListener(e -> {
+            // checks for number of tasks complete
             int numChecked = 0;
-            for (int i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].isSelected()) {
+            for (JCheckBox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
                     numChecked++;
                 }
             }
             if (numChecked > 0) {
-                int confirm = JOptionPane.showConfirmDialog(mainGUI.this,
+                // checks if you want to confirm
+                int confirm = JOptionPane.showConfirmDialog(todoList.this,
                         "Are you sure you want to mark the selected task(s) as complete?",
                         "Confirm Completion", JOptionPane.YES_NO_OPTION);
                 if (confirm == JOptionPane.YES_OPTION) {
@@ -94,25 +108,31 @@ public class mainGUI extends JFrame {
                             tasks.set(i, "[Complete] " + tasks.get(i));
                         }
                     }
+                    // helper function accounts for tasks which are already complete
                     updateCheckboxes();
                 }
             } else {
-                JOptionPane.showMessageDialog(mainGUI.this,
+                JOptionPane.showMessageDialog(todoList.this,
                         "Please select tasks to mark as complete.", "No Tasks Selected", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
+        // adds the buttons to JPanel
         panel.add(addButton);
         panel.add(deleteButton);
         panel.add(completeButton);
 
+        // adds the panel to the top level container of the GUI
         getContentPane().add(panel);
+        // makes the GUI an appropriate size
         pack();
+        // centers the frame
         setLocationRelativeTo(null);
+        // displays the GUI (no longer behind the curtains)
         setVisible(true);
     }
 
-    // updates the checkboxes?
+    // Keeps the GUI up to date with the underlying list of tasks
     private void updateCheckboxes() {
         getContentPane().removeAll();
         checkboxes = new JCheckBox[tasks.size()];
@@ -126,23 +146,24 @@ public class mainGUI extends JFrame {
 
         JButton addButton = new JButton("Add Task");
         addButton.addActionListener(e -> {
-            String newTask = JOptionPane.showInputDialog(mainGUI.this, "Enter the new task:");
+            String newTask = JOptionPane.showInputDialog(todoList.this, "Enter the new task:");
             if (newTask != null && !newTask.trim().isEmpty()) {
                 tasks.add(newTask);
                 updateCheckboxes();
             }
         });
 
+        // code for deleting the tasks
         JButton deleteButton = new JButton("Delete Selected Tasks");
         deleteButton.addActionListener(e -> {
             int numChecked = 0;
-            for (int i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].isSelected()) {
+            for (JCheckBox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
                     numChecked++;
                 }
             }
             if (numChecked > 0) {
-                int confirm = JOptionPane.showConfirmDialog(mainGUI.this,
+                int confirm = JOptionPane.showConfirmDialog(todoList.this,
                         "Are you sure you want to delete the selected task(s)?",
                         "Confirm Deletion", JOptionPane.YES_NO_OPTION);
                 if (confirm == JOptionPane.YES_OPTION) {
@@ -156,33 +177,34 @@ public class mainGUI extends JFrame {
                     updateCheckboxes();
                 }
             } else {
-                JOptionPane.showMessageDialog(mainGUI.this,
+                JOptionPane.showMessageDialog(todoList.this,
                         "Please select tasks to delete.", "No Tasks Selected", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
+        // code for marking tasks as complete
         JButton completeButton = new JButton("Mark Selected Tasks as Complete");
         completeButton.addActionListener(e -> {
             int numChecked = 0;
-            for (int i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].isSelected()) {
+            for (JCheckBox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
                     numChecked++;
                 }
             }
             if (numChecked > 0) {
-                int confirm = JOptionPane.showConfirmDialog(mainGUI.this,
+                int confirm = JOptionPane.showConfirmDialog(todoList.this,
                         "Are you sure you want to mark the selected task(s) as complete?",
                         "Confirm Completion", JOptionPane.YES_NO_OPTION);
                 if (confirm == JOptionPane.YES_OPTION) {
                     for (int i = 0; i < checkboxes.length; i++) {
-                        if (checkboxes[i].isSelected()) {
+                        if (checkboxes[i].isSelected() && !tasks.get(i).startsWith("[Complete] ")) {
                             tasks.set(i, "[Complete] " + tasks.get(i));
                         }
                     }
                     updateCheckboxes();
                 }
             } else {
-                JOptionPane.showMessageDialog(mainGUI.this,
+                JOptionPane.showMessageDialog(todoList.this,
                         "Please select tasks to mark as complete.", "No Tasks Selected", JOptionPane.INFORMATION_MESSAGE);
             }
         });
@@ -196,9 +218,10 @@ public class mainGUI extends JFrame {
         repaint();
     }
 
-    // Main Function to open GUI
+    // main function
     public static void main(String[] args) {
         String[] items = {"workout", "breakfast", "run"};
-        SwingUtilities.invokeLater(() -> new mainGUI(items));
+        // use a lambda to invoke the GUI
+        SwingUtilities.invokeLater(() -> new todoList(items));
     }
 }
